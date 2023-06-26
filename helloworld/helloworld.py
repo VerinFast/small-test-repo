1af6c7e3e78357df152859e6c5ac3193cb7de672
Sean T. Conrad <sean@startupos.dev>
2023-06-26 10:47:52 -0400

    Line count for sample python

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,3 +1,16 @@
 # This is a test python file
 
 print("Hello World")
+
+print("This Python program")
+
+print("should be measured")
+
+print("to have 9 lines of code.")
+
+x = True
+
+if x:
+    print("x is True")
+else:
+    print("x is not True at all")
